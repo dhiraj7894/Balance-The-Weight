ed6fd0384204f59820ec6a19fc44298507f7221c
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2021-04-08 12:30:17 +0530

    Update

---
+++ Assets/_Script/leftRope.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftRope : MonoBehaviour
+{
+    public static leftRope leftR;
+
+    public float _speed = 1;
+    public float _weight;
+    public float _RightWeight;
+
+    public bool isMovingUp = false;
+
+    public Transform weightCollector;
+    public List<Transform> weights = new List<Transform>(); 
+
+    void Start()
+    {
+        leftR = this;
+    }
+
+
+    void Update()
+    {
+        _RightWeight = rightRope.rightR._weight;
+        ropeMovement();
+        weightAdder();
+    }
+
+    public void weightAdder()
+    {
+        foreach(Transform obj in weightCollector)
+        {
+            if (!weights.Contains(obj))
+            {
+                weights.Add(obj);
+                _weight += obj.gameObject.GetComponent<_weightMovement>().weight;
+            }
+        }
+        
+    }
+
+    void weightChecker()
+    {
+
+    }
+
+    void ropeMovement()
+    {
+        if (isMovingUp)
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        else if (!isMovingUp)
+        {
+            transform.Translate(Vector3.down * -_speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/_Script/rightRope.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightRope : MonoBehaviour
+{
+    public static rightRope rightR;
+
+    public float _speed = 1;
+    public float _weight;
+    public float _LeftWeight;
+
+    public bool isMovingUp = false;
+
+    public Transform weightCollector;
+    public List<Transform> weights = new List<Transform>();
+
+    void Start()
+    {
+        
+        rightR = this;
+    }
+
+
+    void Update()
+    {
+        _LeftWeight = leftRope.leftR._weight;
+        ropeMovement();
+        weightAdder();
+    }
+
+    public void weightAdder()
+    {
+        foreach (Transform obj in weightCollector)
+        {
+            if (!weights.Contains(obj))
+            {
+                weights.Add(obj);
+                _weight += obj.gameObject.GetComponent<_weightMovement>().weight;
+            }
+        }
+
+    }
+
+
+    void ropeMovement()
+    {
+        if (isMovingUp)
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        else if (!isMovingUp)
+        {
+            transform.Translate(Vector3.down * -_speed * Time.deltaTime);
+        }
+    }
+}
